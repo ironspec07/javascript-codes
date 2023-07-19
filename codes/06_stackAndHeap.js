cfb4d674b0e1861e88c96a9a1c6988c418c1c390
ironspec07 <127649008+ironspec07@users.noreply.github.com>
2023-07-19 18:36:43 +0000

    day5

---
+++ codes/06_stackAndHeap.js
@@ -0,0 +1,4 @@
+// all "primitive" data type use "stack" memory
+// all "non-primitive" data type use "heap" memory
+//stack sends a copy of value of variable 
+//heap sends original value of variable 
