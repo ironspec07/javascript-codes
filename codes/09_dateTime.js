531a4a95c157ddda078f1afe8b9ea9ab620838c7
ironspec07 <127649008+ironspec07@users.noreply.github.com>
2023-07-26 20:20:46 +0000

    day8

--- codes/09_dateTime.js
+++ codes/09_dateTime.js
@@ -3,6 +3,9 @@
 // in future there is a preposal to make temporal api an object just like Math object
 
 let myDate = new Date()
+
+//here type of myDate is object.
+
 console.log(myDate.toString());//Wed Jul 26 2023 14:56:55 GMT+0000 (Coordinated Universal Time)
 
 console.log(myDate.toDateString());//Wed Jul 26 2023
@@ -16,4 +19,32 @@ console.log(myDate.toLocaleDateString());// 7/26/2023
 console.log(myDate.toLocaleString());// 7/26/2023, 3:01:41 PM
 
 console.log(myDate.toLocaleTimeString());// 3:02:27 PM
+
+let createdDate = new Date(2023,5,19)//creates desired date (year,month[starting from 0],day)
+console.log(createdDate.toDateString());
+
+let goodDate = new Date("2007-06-19")//here months start from 1
+console.log(goodDate.toDateString());
+
+let myTimeStamp =Date.now()
+console.log(myTimeStamp);//prints time in mili second from 1-1-1970
+
+console.log(goodDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));//to get time in seconds
+
+console.log(goodDate.getDay()); // 2-> tuesday
+console.log(goodDate.getMonth()); // 5=> june (starts from 0)
+console.log(goodDate.getFullYear()); // 2007
+console.log(goodDate.getUTCDate()); // 19
+console.log(goodDate.getDate()); // 19
+console.log(goodDate.getUTCDay()); // 2=>tuesday
+
+console.log(goodDate.toLocaleString('default',{
+    day:"2-digit",//19
+    weekday:"long"//tuesday
+}));
+
+
+
 
---
+++ codes/10_arrays.js
@@ -0,0 +1,13 @@
+//The Array object, as with arrays in other programming languages, enables storing a collection of multiple items under a single variable name, and has members for performing common array operations.
+
+//JavaScript arrays are resizable and can contain a mix of different data types.
+
+//JavaScript arrays are zero-indexed: the first element of an array is at index 0
+
+//JavaScript array-copy operations create shallow copies.
+
+//A shallow copy of an object is a copy whose properties share the same references (point to the same underlying values) as those of the source object from which the copy was made. (change made in copy also change original array)
+
+//A deep copy of an object is a copy whose properties do not share the same references
+
+const myArr = [0, 1, 2, 3, 4, 5]
