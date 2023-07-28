213379e6ebaea7c72fa92919247de75577a831bf
ironspec07 <127649008+ironspec07@users.noreply.github.com>
2023-07-28 18:26:18 +0000

    day10

--- codes/11_objects.js
+++ codes/11_objects.js
@@ -18,3 +18,20 @@ console.log(JsUser.email)
 console.log(JsUser["email"])//another way of declaring array.
 console.log(JsUser["full name"])
 console.log(JsUser[mySym])
+
+
+JsUser.email = "hitesh@chatgpt.com"//changing 
+email
+Object.freeze(JsUser)//freezing object i.e. making values of object fixed
+JsUser.email = "hitesh@microsoft.com"
+console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
