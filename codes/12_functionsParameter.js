48067ad22c96157870417fb9fada7e7766982c60
ironspec07 <127649008+ironspec07@users.noreply.github.com>
2023-07-29 20:58:29 +0000

    day12

---
+++ codes/12_functionsParameter.js
@@ -0,0 +1,21 @@
+function sayMyName(){
+    console.log("A");
+    console.log("D");
+    console.log("I");
+    console.log("T");
+    console.log("Y");
+    console.log("A");
+}
+
+//sayMyName --> FUNCTION REFERENCE
+//sayMyName() --> FUNCTION EXECUTION
+
+sayMyName()
+
+function addTwoNumbers(number1,number2){
+    console.log(number1+number2);
+}
+
+addTwoNumbers()//returns NaN as no argument is passed
+addTwoNumbers(4,3)//returns 7
+
