c5bcdb36befa0f27aa0cad009e5ee9f556f2e163
ironspec07 <127649008+ironspec07@users.noreply.github.com>
2023-08-16 19:31:42 +0000

    day13

--- codes/12_functionsParameter.js
+++ codes/12_functionsParameter.js
@@ -12,10 +12,31 @@ function sayMyName(){
 
 sayMyName()
 
-function addTwoNumbers(number1,number2){
+function addTwoNumbers(number1,number2){//parameters
     console.log(number1+number2);
 }
 
 addTwoNumbers()//returns NaN as no argument is passed
-addTwoNumbers(4,3)//returns 7
+addTwoNumbers(4,3)//returns 7 // arguments 
 
+// function addTwoNumbers(number1,number2){
+//     console.log(number1+number2);
+// }
+
+const result = addTwoNumbers(3,5)
+
+console.log(result);// returns - undefined
+
+function addTwoNumbers(number1,number2){
+    
+    let result = number1+number2;
+    return result
+    console.log("code");//unreachable code as nothing executes after return
+}
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("suhu"));
+console.log(loginUserMessage());//when u don't pass anything the output will be "undefined just logged in" 
