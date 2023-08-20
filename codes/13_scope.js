d916bb54ae0f334bb7d9561d6e71fe44cb0be4ce
ironspec07 <127649008+ironspec07@users.noreply.github.com>
2023-08-20 13:19:22 +0000

    day15

---
+++ codes/13_scope.js
@@ -0,0 +1,69 @@
+let x = 10
+const b = 20
+var c = 30
+
+console.log(x); //10 
+console.log(b); //20
+console.log(c); //30
+
+//{} --> scope
+
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "Adii"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "Adii"
+    if (username === "Adii") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
---
+++ codes/14_this_arrowFunction.js
@@ -0,0 +1,11 @@
+const user = {
+    username: "aditya",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to our wensite `);
+    }
+}
+
+//this --> keyword is used to define current context
+
