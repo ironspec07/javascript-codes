16cdc9261065fe9d7f683755aeb2a40843fd3c57
ironspec <ironspec07@gmail.com>
2023-08-26 20:22:17 +0530

    day17

--- codes/16_controlFlow.js
+++ codes/16_controlFlow.js
@@ -68,7 +68,7 @@ switch (month) {
         break;
     case "feb":
         console.log("feb");
-        break;
+        break; // javaScript runs all the code if a case matches except default if break statement is not present
     case "march":
         console.log("march");
         break;
@@ -80,3 +80,47 @@ switch (month) {
         console.log("default case match");
         break;
 }
+//****************************************TRUTHY & FALSY**********************************//
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+if (userEmail.length === 0) {
+    console.log("Array is empty");
+}// to check if an array is empty.
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}//to check if an object is empty
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
