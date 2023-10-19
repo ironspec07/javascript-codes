ba8777837554b78ce7e6938fb7791ce2b6ced314
ironspec07 <ironspec07@gmail.com>
2023-10-19 19:13:59 +0530

    day33

---
+++ codes/35_inheritance.js
@@ -0,0 +1,31 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username) // In JavaScript, the super keyword is used to call a parent class's constructor or methods from within a subclass. It allows you to access and invoke the functionalities of the parent class.
+        
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.logMe()
+const masalaChai = new User("masalaChai")
+
+masalaChai.logMe()
+
+console.log(chai instanceof User);
---
+++ codes/36_staticProperties.js
@@ -0,0 +1,28 @@
+//In JavaScript, the `static` keyword is used to define a method or property that is associated with the class itself, rather than with instances of the class. It can be accessed directly on the class without creating an object.
+
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
