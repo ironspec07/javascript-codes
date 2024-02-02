cab035c09d96e4b83d9d3f6c7aadd394e93e99c8
ironspec07 <ironspec07@gmail.com>
2024-02-02 16:58:08 +0530

    Scrimba : Passenger Counder Day-2

--- Scrimba/Passenger Counter/script.js
+++ Scrimba/Passenger Counter/script.js
@@ -1,3 +1,15 @@
 // document.getElementById("count-el").innerText = 5
 
 let count = 0
+
+function increment() {
+    count++
+    document.getElementById("count-el").innerText = count
+}
+function decrement() {
+    count--
+    document.getElementById("count-el").innerText = count
+}
+function save() {
+    console.log(count);
+}
