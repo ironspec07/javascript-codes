d4a947f0505c0d6e5061efebf93b30d0c5a09a08
ironspec07 <ironspec07@gmail.com>
2024-02-12 20:05:24 +0530

    Scrimba : Black Jack Game Day-2

--- Scrimba/BlackJackgame/index.js
+++ Scrimba/BlackJackgame/index.js
@@ -1,35 +1,56 @@
-let firstCard = 2+ Math.floor((Math.random()*10)) 
+let firstCard = 2 + Math.floor(Math.random() * 10);
 
-let secondCard = 2+ Math.floor((Math.random()*10))
+let secondCard = 2 + Math.floor(Math.random() * 10);
 
-let sum = firstCard+secondCard
+let sum = firstCard + secondCard;
 
-let hasBlackjack = false
-let isAlive = true
-let message = ""
+let hasBlackjack = false;
+let isAlive = true;
+let message = "";
 
-let message_el = document.getElementById("message-el")
-let sum_el = document.getElementById("sum-el")
-let cards_el = document.getElementById("cards-el")
-
+let message_el = document.getElementById("message-el");
+let sum_el = document.getElementById("sum-el");
+let cards_el = document.getElementById("cards-el");
 
 function startGame() {
-    
-    cards_el.textContent += " " + firstCard + " ," + " " + secondCard 
+    cards_el.textContent += " " + firstCard + " ," + " " + secondCard;
 
-    sum_el.textContent += " "+sum
-
+    sum_el.textContent += " " + sum;
 
     if (sum < 21) {
-        message = "Do you want to draw a new card? 🙂"
+    message = "Do you want to draw a new card? 🙂";
+    } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackjack = true;
+    } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
     }
-    else if (sum === 21) {
-        message =  "Wohoo! You've got Blackjack! 🥳" 
-        hasBlackjack = true
+    message_el.textContent = message;
+    document.getElementById("start-btn").disabled = true;
+}
+function newCard() {
+  let thirdCard = 2 + Math.floor(Math.random() * 10);
+
+    cards_el.textContent += ", " + thirdCard;
+
+    sum += thirdCard;
+
+    sum_el.textContent = "Sum: " + sum;
+
+    if (sum < 21) {
+    message = "Do you want to draw a new card? 🙂";
+    } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackjack = true;
+    document.getElementById("new-btn").disabled = true;
+    } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+    document.getElementById("new-btn").disabled = true;
     }
-    else {
-        message = "You're out of the game! 😭" 
-        isAlive = false
-    }
-    message_el.textContent = message 
+    message_el.textContent = message;
+}
+function resetGame() {
+    location.reload();
 }
